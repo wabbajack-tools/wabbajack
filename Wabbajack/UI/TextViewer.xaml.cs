9b428aefdcb17f7435963f6a8d2fb33748f035ba
erri120 <erri120@protonmail.com>
2019-10-11 14:57:30 +0200

    Created TextViewer

---
+++ Wabbajack/UI/TextViewer.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Wabbajack.UI
+{
+    public partial class TextViewer : Window
+    {
+        public TextViewer(string text)
+        {
+            InitializeComponent();
+            TextBlock.Text = text;
+            
+        }
+    }
+}
