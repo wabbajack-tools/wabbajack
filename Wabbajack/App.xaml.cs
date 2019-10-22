04f9775e96351f08add09d7ce4644d044befd62f
Justin Swanson <justin.c.swanson@gmail.com>
2019-10-21 22:51:55 -0500

    Removed some vestigial code not being called

--- Wabbajack/App.xaml.cs
+++ Wabbajack/App.xaml.cs
@@ -10,36 +10,5 @@ namespace Wabbajack
     /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            /*
-            Utils.Log($"Wabbajack Build - {ThisAssembly.Git.Sha}");
-            SetupHandlers();
-
-            var args = Environment.GetCommandLineArgs();
-            if (args.Length > 1)
-            {
-                Utils.SetLoggerFn(f => { });
-                WorkQueue.Init((a, b, c) => { }, (a, b) => { });
-                var updater = new CheckForUpdates(args[1]);
-                if (updater.FindOutdatedMods())
-                {
-                    Environment.Exit(0);
-                }
-                Environment.Exit(1);
-            }*/
-
-        }
-
-        private void SetupHandlers()
-        {
-            AppDomain.CurrentDomain.UnhandledException += AppHandler;
-        }
-
-        private void AppHandler(object sender, UnhandledExceptionEventArgs e)
-        {
-            Utils.Log("Uncaught error:");
-            Utils.Log(((Exception)e.ExceptionObject).ExceptionToString());
-        }
     }
 }
