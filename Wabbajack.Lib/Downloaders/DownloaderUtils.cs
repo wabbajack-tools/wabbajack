35f57307375f42d4c35e984a4b0432327f7217ae
Timothy Baldridge <tbaldridge@gmail.com>
2019-10-23 19:22:11 -0600

    add files I forgot

---
+++ Wabbajack.Lib/Downloaders/DownloaderUtils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wabbajack.Lib.Downloaders
+{
+    public static class DownloaderUtils
+    {
+        public static Uri GetDirectURL(dynamic meta)
+        {
+            var url = meta?.General?.directURL;
+            if (url == null) return null;
+            
+            return Uri.TryCreate((string) url, UriKind.Absolute, out var result) ? result : null;
+        }
+    }
+}
