2f8977feac1e118b2596f826630a6ba67aa7ff69
Justin Swanson <justin.c.swanson@gmail.com>
2019-10-30 14:39:12 -0500

    Some additional library items
    UserControlRx, FilePicker control, TaskExt.FireAndForget

---
+++ Wabbajack.Lib/Extensions/TaskExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wabbajack
+{
+    public static class TaskExt
+    {
+        public static async void FireAndForget(this Task task, Action<Exception> onException = null)
+        {
+            try
+            {
+                await task.ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            when (onException != null)
+            {
+                onException(ex);
+            }
+        }
+    }
+}
---
+++ Wabbajack/Views/UserControlRx.cs
@@ -0,0 +1,48 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wabbajack
+{
+    public class UserControlRx : UserControl, IDisposable, IReactiveObject
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        public void RaisePropertyChanging(PropertyChangingEventArgs args)
+        {
+            PropertyChanging?.Invoke(this, args);
+        }
+
+        public void RaisePropertyChanged(PropertyChangedEventArgs args)
+        {
+            PropertyChanged?.Invoke(this, args);
+        }
+
+        private readonly Lazy<CompositeDisposable> _CompositeDisposable = new Lazy<CompositeDisposable>();
+        public CompositeDisposable CompositeDisposable => _CompositeDisposable.Value;
+
+        public virtual void Dispose()
+        {
+            if (_CompositeDisposable.IsValueCreated)
+            {
+                _CompositeDisposable.Value.Dispose();
+            }
+        }
+
+        protected static void WireNotifyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (!(d is UserControlRx control)) return;
+            if (object.Equals(e.OldValue, e.NewValue)) return;
+            control.RaisePropertyChanged(e.Property.Name);
+        }
+    }
+}
