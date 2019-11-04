85d72968c33a765983ef5e8fdcdf7075b7c5f8b2
Timothy Baldridge <tbaldridge@gmail.com>
2019-11-04 15:09:11 -0700

    reorder known types to not break old lists

--- Wabbajack.Lib/CerasConfig.cs
+++ Wabbajack.Lib/CerasConfig.cs
@@ -16,14 +16,14 @@ namespace Wabbajack.Lib
                 typeof(NoMatch), typeof(InlineFile), typeof(PropertyType), typeof(CleanedESM),
                 typeof(RemappedInlineFile), typeof(FromArchive), typeof(CreateBSA), typeof(PatchedFromArchive),
                 typeof(SourcePatch), typeof(MergedPatch), typeof(Archive), typeof(IndexedArchive), typeof(IndexedEntry),
-                typeof(ArchiveMeta),
                 typeof(IndexedArchiveEntry), typeof(BSAIndexedEntry), typeof(VirtualFile),
                 typeof(ArchiveStateObject), typeof(FileStateObject), typeof(IDownloader),
                 typeof(IUrlDownloader), typeof(AbstractDownloadState), typeof(ManualDownloader.State),
                 typeof(DropboxDownloader), typeof(GoogleDriveDownloader.State), typeof(HTTPDownloader.State),
                 typeof(MegaDownloader.State), typeof(ModDBDownloader.State), typeof(NexusDownloader.State),
                 typeof(BSAStateObject), typeof(BSAFileStateObject), typeof(BA2StateObject), typeof(BA2DX10EntryState),
-                typeof(BA2FileEntryState), typeof(MediaFireDownloader.State)
+                typeof(BA2FileEntryState), typeof(MediaFireDownloader.State), typeof(ArchiveMeta),
+
             }
         };
     }
