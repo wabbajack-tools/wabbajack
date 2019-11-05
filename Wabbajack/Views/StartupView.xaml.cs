301a37993791da5814516c7afc0bf750836de3a2
erri120 <erri120@protonmail.com>
2019-11-05 18:37:18 +0100

    Added ScrollViewer and new Banner to Startup

--- Wabbajack/Views/StartupView.xaml.cs
+++ Wabbajack/Views/StartupView.xaml.cs
@@ -8,7 +8,8 @@ namespace Wabbajack.Views
         public StartupView()
         {
             InitializeComponent();
-            Banner.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.banner_small_dark.png");
+            //Banner.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.banner_small_dark.png");
+            Banner.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.Wabba_Mouth.png");
         }
     }
 }
