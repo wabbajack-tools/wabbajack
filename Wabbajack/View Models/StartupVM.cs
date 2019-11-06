fe2522f7387914195588d0fd71a2d006ae1aa1b1
erri120 <erri120@protonmail.com>
2019-11-06 18:30:27 +0100

    Added ReactiveCommands for installing and compiling

--- Wabbajack/View Models/StartupVM.cs
+++ Wabbajack/View Models/StartupVM.cs
@@ -14,8 +14,8 @@ namespace Wabbajack
         {
             _mainWindow = mainWindow;
             OpenModListGallery = ReactiveCommand.Create(() => { _mainWindow.Page = 1; });
+            InstallFromFile = ReactiveCommand.Create(() => { _mainWindow.Page = 2; });
+            CompileModList = ReactiveCommand.Create(() => { _mainWindow.Page = 3; });
         }
-
-        
     }
 }
