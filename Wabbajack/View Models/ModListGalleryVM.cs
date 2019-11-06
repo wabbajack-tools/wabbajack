a21b8ec761152d290e3b1add147ace24b4dc6d68
erri120 <erri120@protonmail.com>
2019-11-06 15:07:30 +0100

    Removed unused files and imports

--- Wabbajack/View Models/ModListGalleryVM.cs
+++ Wabbajack/View Models/ModListGalleryVM.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using DynamicData.Binding;
 using ReactiveUI.Fody.Helpers;
-using Wabbajack.Common;
 using Wabbajack.Lib;
 using Wabbajack.Lib.ModListRegistry;
 
--- Wabbajack/Views/ModListGalleryView.xaml.cs
+++ Wabbajack/Views/ModListGalleryView.xaml.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Wabbajack.Views
 {
