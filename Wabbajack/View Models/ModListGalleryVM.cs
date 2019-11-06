c265b19d3e38c4d9968c2af0730e6f15729fbf5e
erri120 <erri120@protonmail.com>
2019-11-06 12:06:27 +0100

    Created ModListGallery

---
+++ Wabbajack/View Models/ModListGalleryVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicData.Binding;
+using ReactiveUI.Fody.Helpers;
+using Wabbajack.Lib;
+using Wabbajack.Lib.ModListRegistry;
+
+namespace Wabbajack
+{
+    public class ModListGalleryVM : ViewModel
+    {
+        
+        public ObservableCollection<ModlistMetadata> ModLists { get; } = new ObservableCollectionExtended<ModlistMetadata>(ModlistMetadata.LoadFromGithub());
+
+        public string TestImage => ModLists[0].Links.ImageUri;
+        public string TestTitle => $"{ModLists[0].Title} by {ModLists[0].Author}";
+        public string TestDescription => ModLists[0].Description;
+        public string TestGame => ModLists[0].Game.ToString();
+
+        public ModListGalleryVM()
+        {
+
+        }
+    }
+}
---
+++ Wabbajack/Views/ModListGalleryView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace Wabbajack.Views
+{
+    public partial class ModListGalleryView : UserControl
+    {
+        public ModListGalleryView()
+        {
+            InitializeComponent();
+            DataContext = new ModListGalleryVM();
+        }
+    }
+}
