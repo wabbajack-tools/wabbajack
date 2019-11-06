49fde4ba8b5a48daf6cfbbe69c550aa632e64a68
erri120 <erri120@protonmail.com>
2019-11-06 16:42:35 +0100

    Added MO2 and Vortex buttons

--- Wabbajack/Views/StartupView.xaml.cs
+++ Wabbajack/Views/StartupView.xaml.cs
@@ -10,6 +10,8 @@ namespace Wabbajack.Views
             InitializeComponent();
             //Banner.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.banner_small_dark.png");
             Banner.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.Wabba_Mouth.png");
+            MO2Image.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.MO2Button.png");
+            VortexImage.Source = UIUtils.BitmapImageFromResource("Wabbajack.Resources.VortexButton.png");
         }
     }
 }
