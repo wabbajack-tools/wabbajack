60620886817e3efc934dd3921b7f49a58c712f53
erri120 <erri120@protonmail.com>
2019-11-09 14:51:51 +0100

    Removed unused imports

--- Wabbajack.Common/GameMetaData.cs
+++ Wabbajack.Common/GameMetaData.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
 using Alphaleonis.Win32.Filesystem;
 using Microsoft.Win32;
 
