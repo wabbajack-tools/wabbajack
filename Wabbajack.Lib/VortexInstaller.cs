3ad63bda39430e441d4efebed19fa48f81791389
erri120 <erri120@protonmail.com>
2019-11-09 21:40:25 +0100

    Created VortexInstaller

---
+++ Wabbajack.Lib/VortexInstaller.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VFS;
+using Wabbajack.Common;
+
+namespace Wabbajack.Lib
+{
+    public class VortexInstaller
+    {
+        public string ModListArchive { get; }
+        public ModList ModList { get; }
+
+        public VirtualFileSystem VFS => VirtualFileSystem.VFS;
+
+        public VortexInstaller(string archive, ModList modList)
+        {
+            ModListArchive = archive;
+            ModList = modList;
+        }
+
+        public void Info(string msg)
+        {
+            Utils.Log(msg);
+        }
+
+        public void Status(string msg)
+        {
+            WorkQueue.Report(msg, 0);
+        }
+
+        private void Error(string msg)
+        {
+            Utils.Log(msg);
+            throw new Exception(msg);
+        }
+
+        public byte[] LoadBytesFromPath(string path)
+        {
+            using (var fs = new FileStream(ModListArchive, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var ar = new ZipArchive(fs, ZipArchiveMode.Read))
+            using (var ms = new MemoryStream())
+            {
+                var entry = ar.GetEntry(path);
+                using (var e = entry.Open())
+                    e.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        public static ModList LoadFromFile(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (var ar = new ZipArchive(fs, ZipArchiveMode.Read))
+            {
+                var entry = ar.GetEntry("modlist");
+                if (entry == null)
+                {
+                    entry = ar.GetEntry("modlist.json");
+                    using (var e = entry.Open())
+                        return e.FromJSON<ModList>();
+                }
+                using (var e = entry.Open())
+                    return e.FromCERAS<ModList>(ref CerasConfig.Config);
+            }
+        }
+    }
+}
