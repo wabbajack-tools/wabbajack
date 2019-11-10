451dee01b63fbf2142ded08d0afbfa768d8daf6e
Justin Swanson <justin.c.swanson@gmail.com>
2019-11-09 19:29:00 -0600

    Fix to disable inputs when compiling

---
+++ Wabbajack/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wabbajack
+{
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException($"The target must be of type bool");
+            return !((bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException($"The target must be of type bool");
+            return !((bool)value);
+        }
+    }
+}
