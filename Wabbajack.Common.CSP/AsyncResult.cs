63193acbe2cb5848a694062d62948d6a7419c94c
Timothy Baldridge <tbaldridge@gmail.com>
2019-11-11 22:05:53 -0700

    Break the Channel interface into Read/Write/Close ports

---
+++ Wabbajack.Common.CSP/AsyncResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wabbajack.Common.CSP
+{
+    public enum AsyncResult : int
+    {
+        /// <summary>
+        /// The channel was closed, so the returned value is meaningless
+        /// </summary>
+        Closed,
+
+        /// <summary>
+        /// The handler was canceled, so the returned value is meaningless
+        /// </summary>
+        Canceled,
+
+        /// <summary>
+        /// The callback was enqueued into the pending operations buffer, return value is useless
+        /// </summary>
+        Enqueued,
+
+        /// <summary>
+        /// The operation passed on the current thread, return the current value as the response value
+        /// </summary>
+        Completed
+    }
+}
---
+++ Wabbajack.Common.CSP/IWritePort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wabbajack.Common.CSP
+{
+    public interface IWritePort<TIn> : ICloseable
+    {
+        (AsyncResult, bool) Put(TIn val, Handler<Action<bool>> handler);
+        ValueTask<bool> Put(TIn val, bool onCaller = true);
+    }
+}
