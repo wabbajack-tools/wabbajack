a9fcc715d29dd3d05e00ee3eb1aa85ad4fbdd60f
erri120 <erri120@protonmail.com>
2019-11-21 15:26:25 +0100

    Removed unused imports from Wabbajack.Common.CSP

--- Wabbajack.Common.CSP/FixedSizeBuffer.cs
+++ Wabbajack.Common.CSP/FixedSizeBuffer.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Wabbajack.Common.CSP
+﻿namespace Wabbajack.Common.CSP
 {
     public class FixedSizeBuffer<T> : IBuffer<T>
     {
--- Wabbajack.Common.CSP/Handler.cs
+++ Wabbajack.Common.CSP/Handler.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading.Tasks.Sources;
-
-namespace Wabbajack.Common.CSP
+﻿namespace Wabbajack.Common.CSP
 {
     public interface Handler<T>
     {
