44feef19853cdd99830f1cdd5f84f3565cb7809e
erri120 <erri120@protonmail.com>
2019-11-21 15:26:45 +0100

    Removed unused imports from Wabbajack.CacheServer

--- Wabbajack.CacheServer/Server.cs
+++ Wabbajack.CacheServer/Server.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
-using Nancy.Hosting.Self;
 
 namespace Wabbajack.CacheServer
 {
