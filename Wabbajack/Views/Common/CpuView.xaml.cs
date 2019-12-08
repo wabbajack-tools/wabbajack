886fbd13ad6cc8ade9f19079b18546a04b1331b8
Justin Swanson <justin.c.swanson@gmail.com>
2019-12-08 16:30:49 -0600

    Split LogCpuView

---
+++ Wabbajack/Views/Common/CpuView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wabbajack
+{
+    /// <summary>
+    /// Interaction logic for CpuView.xaml
+    /// </summary>
+    public partial class CpuView : UserControl
+    {
+        public double ProgressPercent
+        {
+            get => (double)GetValue(ProgressPercentProperty);
+            set => SetValue(ProgressPercentProperty, value);
+        }
+        public static readonly DependencyProperty ProgressPercentProperty = DependencyProperty.Register(nameof(ProgressPercent), typeof(double), typeof(CpuView),
+             new FrameworkPropertyMetadata(default(double)));
+
+        public CpuView()
+        {
+            InitializeComponent();
+        }
+    }
+}
