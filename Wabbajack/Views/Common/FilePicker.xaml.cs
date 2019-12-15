e388c018dc6834f2055a7e204ffb3e0aded8cd39
Justin Swanson <justin.c.swanson@gmail.com>
2019-12-15 00:20:28 -0600

    Fixed obscure FilePicker bug
    The exit animations on errors were not firing if the datacontext was what changed.  Adding a middleman property fixed the issue.  Probably a bit hacky

--- Wabbajack/Views/Common/FilePicker.xaml.cs
+++ Wabbajack/Views/Common/FilePicker.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Wabbajack.Lib;
 
 namespace Wabbajack
 {
@@ -7,6 +10,16 @@ namespace Wabbajack
     /// </summary>
     public partial class FilePicker : UserControl
     {
+        // This exists, as utilizing the datacontext directly seemed to bug out the exit animations
+        // "Bouncing" off this property seems to fix it, though.  Could perhaps be done other ways.
+        public FilePickerVM PickerVM
+        {
+            get => (FilePickerVM)GetValue(PickerVMProperty);
+            set => SetValue(PickerVMProperty, value);
+        }
+        public static readonly DependencyProperty PickerVMProperty = DependencyProperty.Register(nameof(PickerVM), typeof(FilePickerVM), typeof(FilePicker),
+             new FrameworkPropertyMetadata(default(FilePickerVM)));
+
         public FilePicker()
         {
             InitializeComponent();
