61474e5bc5ddd4f0e2b7fd86b7d69b62b5d42690
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-02 14:38:24 -0700

    Switch cache server to 64bit, fix bad primary key in metrics DTO

--- Wabbajack.CacheServer/DTOs/Metric.cs
+++ Wabbajack.CacheServer/DTOs/Metric.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 
 namespace Wabbajack.CacheServer.DTOs
 {
     public class Metric
     {
+        [BsonId]
+        public ObjectId Id;
         public DateTime Timestamp;
         public string Action;
         public string Subject;
