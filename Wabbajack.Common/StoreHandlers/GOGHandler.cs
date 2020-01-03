62c3178ff259bdf0dcc1c70e06f34a8d72ecc5f7
erri120 <erri120@protonmail.com>
2020-01-03 18:03:09 +0100

    Overhauled the StoreHandlers

---
+++ Wabbajack.Common/StoreHandlers/GOGHandler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using Microsoft.Win32;
+
+namespace Wabbajack.Common.StoreHandlers
+{
+    public class GOGGame : AStoreGame
+    {
+        public override Game Game { get; internal set; }
+        public override string Name { get; internal set; }
+        public override string Path { get; internal set; }
+        public override int ID { get; internal set; }
+        public override StoreType Type { get; internal set; } = StoreType.GOG;
+    }
+
+    public class GOGHandler : AStoreHandler
+    {
+        public override List<AStoreGame> Games { get; set; }
+        public override StoreType Type { get; internal set; }
+
+        private const string GOGRegKey = @"Software\GOG.com\Games";
+        private const string GOG64RegKey = @"Software\WOW6432Node\GOG.com\Games";
+
+        private RegistryKey GOGKey { get; set; }
+
+        public override bool Init()
+        {
+            try
+            {
+                var gogKey = Registry.LocalMachine.OpenSubKey(GOGRegKey) ??
+                             Registry.LocalMachine.OpenSubKey(GOG64RegKey);
+
+                if (gogKey == null)
+                {
+                    Utils.Error(new StoreException("Could not open the GOG registry key!"));
+                    return false;
+                }
+
+                GOGKey = gogKey;
+                return true;
+            }
+            catch (SecurityException se)
+            {
+                Utils.Error(se, "GOGHandler could not read from registry!");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Utils.Error(uae, "GOGHandler could not read from registry!");
+            }
+
+            return false;
+        }
+
+        public override bool LoadAllGames()
+        {
+            try
+            {
+                string[] keys = GOGKey.GetSubKeyNames();
+                Utils.Log($"Found {keys.Length} SubKeys for GOG");
+
+                keys.Do(key =>
+                {
+                    if (!int.TryParse(key, out var gameID))
+                    {
+                        Utils.Error(new StoreException($"Could not read gameID for key {key}"));
+                        return;
+                    }
+
+                    var subKey = GOGKey.OpenSubKey(key);
+                    if (subKey == null)
+                    {
+                        Utils.Error(new StoreException($"Could not open SubKey for {key}"));
+                        return;
+                    }
+
+                    var gameNameValue = subKey.GetValue("GAMENAME");
+                    if (gameNameValue == null)
+                    {
+                        Utils.Error(new StoreException($"Could not get GAMENAME for {gameID} at {key}"));
+                        return;
+                    }
+
+                    var gameName = gameNameValue.ToString();
+
+                    var pathValue = subKey.GetValue("PATH");
+                    if (pathValue == null)
+                    {
+                        Utils.Error(new StoreException($"Could not get PATH for {gameID} at {key}"));
+                        return;
+                    }
+
+                    var path = pathValue.ToString();
+
+                    var game = new GOGGame() {ID = gameID, Name = gameName, Path = path};
+
+                    var gameMeta = GameRegistry.Games.Values.FirstOrDefault(g =>
+                        g.GOGIDs.Contains(game.ID)
+                        &&
+                        g.RequiredFiles.TrueForAll(file =>
+                            File.Exists(Path.Combine(game.Path, file))));
+
+                    if (gameMeta == null)
+                        return;
+
+                    game.Game = gameMeta.Game;
+
+                    Utils.Log($"Found GOG Game: \"{game.Name}\"({game.ID}) at {game.Path}");
+
+                    Games.Add(game);
+                });
+            }
+            catch (SecurityException se)
+            {
+                Utils.Error(se, "GOGHandler could not read from registry!");
+            }
+            catch (UnauthorizedAccessException uae)
+            {
+                Utils.Error(uae, "GOGHandler could not read from registry!");
+            }
+            catch (Exception e)
+            {
+                Utils.ErrorThrow(e);
+            }
+
+            Utils.Log($"Total number of GOG Games found: {Games.Count}");
+
+            return true;
+        }
+    }
+}
---
+++ Wabbajack.Common/StoreHandlers/StoreHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Wabbajack.Common.StoreHandlers
+{
+    public enum StoreType
+    {
+        STEAM,
+        GOG
+    }
+
+    public class StoreHandler
+    {
+        private static readonly Lazy<StoreHandler> instance = new Lazy<StoreHandler>(() => new StoreHandler(), true);
+        public static StoreHandler Instance => instance.Value;
+
+        private static readonly Lazy<SteamHandler> _steamHandler = new Lazy<SteamHandler>(() => new SteamHandler());
+        public SteamHandler SteamHandler = _steamHandler.Value;
+
+        private static readonly Lazy<GOGHandler> _gogHandler = new Lazy<GOGHandler>(() => new GOGHandler());
+        public GOGHandler GOGHandler = _gogHandler.Value;
+
+        public List<AStoreGame> StoreGames;
+
+        public StoreHandler()
+        {
+            StoreGames = new List<AStoreGame>();
+
+            if (SteamHandler.Init())
+            {
+                if(SteamHandler.LoadAllGames())
+                    StoreGames.AddRange(SteamHandler.Games);
+                else
+                    Utils.Error(new StoreException("Could not load all Games from the SteamHandler, check previous error messages!"));
+            }
+            else
+            {
+                Utils.Error(new StoreException("Could not Init the SteamHandler, check previous error messages!"));
+            }
+
+            if (GOGHandler.Init())
+            {
+                if(GOGHandler.LoadAllGames())
+                    StoreGames.AddRange(GOGHandler.Games);
+                else
+                    Utils.Error(new StoreException("Could not load all Games from the GOGHandler, check previous error messages!"));
+            }
+            else
+            {
+                Utils.Error(new StoreException("Could not Init the GOGHandler, check previous error messages!"));
+            }
+        }
+
+        public string GetGamePath(Game game)
+        {
+            return StoreGames.FirstOrDefault(g => g.Game == game)?.Path;
+        }
+
+        public string GetGamePath(int id)
+        {
+            return StoreGames.FirstOrDefault(g => g.ID == id)?.Path;
+        }
+    }
+
+    public abstract class AStoreGame
+    {
+        public abstract Game Game { get; internal set; }
+        public abstract string Name { get; internal set; }
+        public abstract string Path { get; internal set; }
+        public abstract int ID { get; internal set; }
+        public abstract StoreType Type { get; internal set; }
+    }
+
+    public abstract class AStoreHandler
+    {
+        public abstract List<AStoreGame> Games { get; set; }
+
+        public abstract StoreType Type { get; internal set; }
+
+        public abstract bool Init();
+
+        public abstract bool LoadAllGames();
+    }
+
+    public class StoreException : Exception
+    {
+        public StoreException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
