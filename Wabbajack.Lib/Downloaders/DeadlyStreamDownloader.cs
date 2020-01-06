91cfe359a8ccc36dd9956273772b7839fdc58c14
erri120 <erri120@protonmail.com>
2020-01-06 16:08:21 +0100

    Created DeadlyStreamDownloader

---
+++ Wabbajack.Lib/Downloaders/DeadlyStreamDownloader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Wabbajack.Lib.Downloaders
+{
+    public class DeadlyStreamDownloader : AbstractIPS4Downloader<DeadlyStreamDownloader, DeadlyStreamDownloader.State>
+    {
+        #region INeedsDownload
+        public override string SiteName => "Deadly Stream";
+        public override Uri SiteURL => new Uri("https://www.deadlystream.com");
+        public override Uri IconUri => new Uri("https://www.deadlystream.com/favicon.ico");
+        #endregion
+
+        public DeadlyStreamDownloader() : base(new Uri("https://deadlystream.com/login"), "deadlystream",
+            "deadlystream.com")
+        {
+
+        }
+
+        public class State : State<DeadlyStreamDownloader>
+        {
+
+        }
+    }
+}
