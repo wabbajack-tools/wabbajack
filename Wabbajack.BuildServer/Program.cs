ad5c8b83e66dfc5cf31b21b92e098cfd0f5e2d99
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-09 16:47:53 -0700

    Bind to all interfaces

--- Wabbajack.BuildServer/Program.cs
+++ Wabbajack.BuildServer/Program.cs
@@ -20,6 +20,7 @@ namespace Wabbajack.BuildServer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://*:5000", "https://*:5001");
                     webBuilder.UseStartup<Startup>();
                 });
     }
