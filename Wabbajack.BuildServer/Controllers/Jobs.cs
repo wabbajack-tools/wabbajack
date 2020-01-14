af9f0dbf89583a765fd90978a5c92da0c81c91b8
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-14 06:35:50 -0700

    Can manually enqueue jobs

--- Wabbajack.BuildServer/Controllers/Jobs.cs
+++ Wabbajack.BuildServer/Controllers/Jobs.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -25,6 +26,16 @@ namespace Wabbajack.BuildServer.Controllers
                 .Where(j => j.Ended == null)
                 .OrderByDescending(j => j.Priority)
                 .ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("enqueue_job/{JobName}")]
+        public async Task<string> EnqueueJob(string JobName)
+        {
+            var jobtype = AJobPayload.NameToType[JobName];
+            var job = new Job{Priority = Job.JobPriority.High, Payload = (AJobPayload)jobtype.GetConstructor(new Type[0]).Invoke(new object?[0])};
+            await Db.Jobs.InsertOneAsync(job);
+            return job.Id;
         }
     }
 }
