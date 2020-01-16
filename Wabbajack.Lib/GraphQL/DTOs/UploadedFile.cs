476d6363db6c5bc14cb45ab49d9d700def438306
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-15 22:32:30 -0700

    GraphQL client support for querying uploaded files

---
+++ Wabbajack.Lib/GraphQL/DTOs/UploadedFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Wabbajack.Lib.GraphQL.DTOs
+{
+    public class UploadedFile
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string MungedName { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Uploader { get; set; }
+        public Uri Uri { get; set; }
+        public string Hash { get; set; }
+        public long Size { get; set; }
+    }
+}
