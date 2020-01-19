141ff50dbee430b39fc2e9082f00247d23f30193
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-19 15:45:18 -0700

    Add missing file

---
+++ Wabbajack/Views/Settings/AuthorFilesView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using ReactiveUI;
+
+namespace Wabbajack
+{
+    public partial class AuthorFilesView : ReactiveUserControl<AuthorFilesVM>
+    {
+        public AuthorFilesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
