dbce33fb4509c49b5817d3fcc68fd39f8448c737
Justin Swanson <justin.c.swanson@gmail.com>
2020-01-18 22:01:51 -0600

    ModListGallery loading ring for loading modlist meta

--- Wabbajack/Views/ModListGalleryView.xaml.cs
+++ Wabbajack/Views/ModListGalleryView.xaml.cs
@@ -1,16 +1,33 @@
 ﻿using System.Diagnostics;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using MahApps.Metro.Controls;
+using ReactiveUI;
 using Wabbajack.Lib.ModListRegistry;
 
 namespace Wabbajack
 {
-    public partial class ModListGalleryView : UserControl
+    public partial class ModListGalleryView : ReactiveUserControl<ModListGalleryVM>
     {
         public ModListGalleryView()
         {
             InitializeComponent();
+
+            this.WhenActivated(dispose =>
+            {
+                this.WhenAny(x => x.ViewModel.BackCommand)
+                    .BindToStrict(this, x => x.BackButton.Command)
+                    .DisposeWith(dispose);
+                this.WhenAny(x => x.ViewModel.ModLists)
+                    .BindToStrict(this, x => x.ModListGalleryControl.ItemsSource)
+                    .DisposeWith(dispose);
+                this.WhenAny(x => x.ViewModel.ModLists.Count)
+                    .Select(x => x > 0 ? Visibility.Collapsed : Visibility.Visible)
+                    .BindToStrict(this, x => x.LoadingRing.Visibility)
+                    .DisposeWith(dispose);
+            });
         }
     }
 }
