898984322e9b4baa11f2ef7f72ba79478ccd2eec
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-25 21:50:52 -0700

    Switch the indexed archives backend to MSSQL for better ACID support

---
+++ Wabbajack.BuildServer/Models/Sql/ArchiveContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wabbajack.BuildServer.Model.Models
+{
+    public partial class ArchiveContent
+    {
+        public long Parent { get; set; }
+        public long Child { get; set; }
+        public string Path { get; set; }
+        public byte[] PathHash { get; set; }
+    }
+}
