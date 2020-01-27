53d8da57873d7f20bcfd63dab79c2edaca044019
erri120 <erri120@protonmail.com>
2020-01-27 14:57:47 +0100

    Created ReactiveMetro class

---
+++ Wabbajack/UI/ReactiveMetro.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using ReactiveUI;
+
+namespace Wabbajack.UI
+{
+    public class ReactiveMetro<TViewModel> : MetroWindow, IViewFor<TViewModel> where TViewModel : class
+    {
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(
+                "ViewModel",
+                typeof(TViewModel),
+                typeof(ReactiveWindow<TViewModel>),
+                new PropertyMetadata(null));
+
+        public TViewModel BindingRoot => ViewModel;
+
+        public TViewModel ViewModel
+        {
+            get => (TViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (TViewModel)value;
+        }
+    }
+}
