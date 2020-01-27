60ec9424fd7f884a45cd5d569a80df9560534d58
erri120 <erri120@protonmail.com>
2020-01-27 15:02:40 +0100

    Added some documentation

--- Wabbajack/UI/ReactiveMetro.cs
+++ Wabbajack/UI/ReactiveMetro.cs
@@ -4,8 +4,23 @@ using ReactiveUI;
 
 namespace Wabbajack.UI
 {
+    /// <summary>
+    /// A <see cref="MetroWindow"/> that is reactive.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This class is a <see cref="MetroWindow"/> that is also reactive. That is, it implements <see cref="IViewFor{TViewModel}"/>.
+    /// You can extend this class to get an implementation of <see cref="IViewFor{TViewModel}"/> rather than writing one yourself.
+    /// </para>
+    /// </remarks>
+    /// <typeparam name="TViewModel">
+    /// The type of the view model backing the view.
+    /// </typeparam>
     public class ReactiveMetro<TViewModel> : MetroWindow, IViewFor<TViewModel> where TViewModel : class
     {
+        /// <summary>
+        /// The view model dependency property.
+        /// </summary>
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register(
                 "ViewModel",
@@ -13,14 +28,19 @@ namespace Wabbajack.UI
                 typeof(ReactiveWindow<TViewModel>),
                 new PropertyMetadata(null));
 
+        /// <summary>
+        /// Gets the binding root view model.
+        /// </summary>
         public TViewModel BindingRoot => ViewModel;
 
+        /// <inheritdoc/>
         public TViewModel ViewModel
         {
             get => (TViewModel)GetValue(ViewModelProperty);
             set => SetValue(ViewModelProperty, value);
         }
 
+        /// <inheritdoc/>
         object IViewFor.ViewModel
         {
             get => ViewModel;
