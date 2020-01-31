aa85c0dc8cfb7c04d20f969c32c684e3eec38829
Timothy Baldridge <tbaldridge@gmail.com>
2020-01-31 15:38:56 -0700

    We have a CLI

---
+++ Wabbajack.CLI/Verbs/Decrypt.cs
@@ -0,0 +1,23 @@
+﻿using Alphaleonis.Win32.Filesystem;
+using CommandLine;
+using Wabbajack.Common;
+
+namespace Wabbajack.CLI.Verbs
+{
+    [Verb("decrypt", HelpText = @"Decrypt data from AppData\Local\Wabbajack and store it locally", Hidden = true)]
+    public class Decrypt
+    {
+        [Option('n', "name", Required = true, HelpText = @"Credential to encrypt and store in AppData\Local\Wabbajack")]
+        public string Name { get; set; }
+
+        
+        [Option('o', "output", Required = true, HelpText = @"Output file for the decrypted data")]
+        public string Output { get; set; }
+
+        public static int Run(Decrypt opts)
+        {
+            File.WriteAllBytes(opts.Output, Utils.FromEncryptedData(opts.Name));
+            return 0;
+        }
+    }
+}
---
+++ Wabbajack.CLI/Verbs/Encrypt.cs
@@ -0,0 +1,22 @@
+﻿using Alphaleonis.Win32.Filesystem;
+using CommandLine;
+using Wabbajack.Common;
+
+namespace Wabbajack.CLI.Verbs
+{
+    [Verb("encrypt", HelpText = @"Encrypt local data and store it in AppData\Local\Wabbajack", Hidden = true)]
+    public class Encrypt
+    {
+        [Option('n', "name", Required = true, HelpText = @"Credential to encrypt and store in AppData\Local\Wabbajack")]
+        public string Name { get; set; }
+        
+        [Option('i', "input", Required = true, HelpText = @"Source data file name")]
+        public string Input { get; set; }
+
+        public static int Run(Encrypt opts)
+        {
+            File.ReadAllBytes(opts.Input).ToEcryptedData(opts.Name);
+            return 0;
+        }
+    }
+}
