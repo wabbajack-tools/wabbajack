ff55bfec9f7c3215ab06e52917d4cf9d1c3e3079
Timothy Baldridge <tbaldridge@gmail.com>
2020-02-15 16:28:20 -0700

    Don't reuse http messages

---
+++ Wabbajack.Test/HttpClientTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wabbajack.Test
+{
+    [TestClass]
+    public class HttpClientTests
+    {
+        [TestMethod]
+        public async Task DoesntReuseHttpMessages()
+        {
+            var client = new Common.Http.Client();
+            // If we reuse the HTTP message this will throw a invalid operation exception
+            await Assert.ThrowsExceptionAsync<HttpRequestException>(async () => await client.GetAsync("http://blerg.blaz.bloz.buz"));
+        }
+    }
+}
