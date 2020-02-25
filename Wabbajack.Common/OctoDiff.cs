4198ec0d64fd7cc56456b38cf82e8f9d96930104
Timothy Baldridge <tbaldridge@gmail.com>
2020-02-25 06:27:50 -0700

    Add a few other checks

--- Wabbajack.Common/OctoDiff.cs
+++ Wabbajack.Common/OctoDiff.cs
@@ -32,7 +32,7 @@ namespace Wabbajack.Common
         {
             public void ReportProgress(string operation, long currentPosition, long total)
             {
-                if (currentPosition >= total)
+                if (currentPosition >= total || total < 1 || currentPosition < 0)
                     return;
                 Utils.Status(operation, new Percent(total, currentPosition));
             }
