276f2f4113a97cbd8d16fd62cc9d0dbc416aaa46
Timothy Baldridge <tbaldridge@gmail.com>
2020-03-03 07:07:30 -0700

    Debug helper for test

--- Wabbajack.Test/DownloaderTests.cs
+++ Wabbajack.Test/DownloaderTests.cs
@@ -467,9 +467,7 @@ namespace Wabbajack.Test
 
             using var tempFile = new TempFile();
             await converted.Download(new Archive {Name = "yt_test.zip"}, tempFile.File.FullName);
-            File.Copy(tempFile.File.FullName, @"c:\tmp\test.zip");
-            
-            Assert.AreEqual("st2QLxOiv8M=", await tempFile.File.FullName.FileHashAsync());
+            Assert.AreEqual("+zUUyo3ROFg=", await tempFile.File.FullName.FileHashAsync());
         }
         
         
