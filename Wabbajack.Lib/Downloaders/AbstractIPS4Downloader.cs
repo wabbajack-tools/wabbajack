28348def0a5ba4d00ef7cd3e0c660219e8214227
erri120 <erri120@protonmail.com>
2020-03-03 17:21:34 +0100

    Implemented new AbstractMetaState for LoversLab

--- Wabbajack.Lib/Downloaders/AbstractIPS4Downloader.cs
+++ Wabbajack.Lib/Downloaders/AbstractIPS4Downloader.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Web;
+using HtmlAgilityPack;
 using Newtonsoft.Json;
 using Wabbajack.Common;
 using Wabbajack.Lib.Validation;
@@ -15,14 +16,12 @@ namespace Wabbajack.Lib.Downloaders
     // IPS4 is the site used by LoversLab, VectorPlexus, etc. the general mechanics of each site are the 
     // same, so we can fairly easily abstract the state.
     // Pass in the state type via TState
-    public abstract class AbstractIPS4Downloader<TDownloader, TState> : AbstractNeedsLoginDownloader, IDownloader 
-        where TState : AbstractIPS4Downloader<TDownloader, TState>.State<TDownloader>, IMetaState, new() 
+    public abstract class AbstractIPS4Downloader<TDownloader, TState> : AbstractNeedsLoginDownloader, IDownloader
+        where TState : AbstractIPS4Downloader<TDownloader, TState>.State<TDownloader>, new()
         where TDownloader : IDownloader
     {
         public override string SiteName { get; }
         public override Uri SiteURL { get; }
-
-        public abstract string ModNameRegex { get; }
 
         public async Task<AbstractDownloadState> GetDownloaderState(dynamic archiveINI)
         {
@@ -63,7 +62,7 @@ namespace Wabbajack.Lib.Downloaders
         }
 
 
-        public class State<TDownloader> : AbstractDownloadState where TDownloader : IDownloader
+        public class State<TDownloader> : AbstractMetaState where TDownloader : IDownloader
         {
             public string FileID { get; set; }
             public string FileName { get; set; }
@@ -75,7 +74,7 @@ namespace Wabbajack.Lib.Downloaders
                 ? $"{URLPrefix}{Downloader.SiteURL.Host}"
                 : Downloader.SiteURL.ToString();
 
-            private static AbstractNeedsLoginDownloader _downloader => (AbstractNeedsLoginDownloader)(object)DownloadDispatcher.GetInstance<TDownloader>();
+            public static AbstractNeedsLoginDownloader Downloader => (AbstractNeedsLoginDownloader)(object)DownloadDispatcher.GetInstance<TDownloader>();
 
             public override object[] PrimaryKey
             {
@@ -110,7 +109,7 @@ namespace Wabbajack.Lib.Downloaders
                 var csrfurl = FileID == null
                     ? $"{Site}/files/file/{FileName}/?do=download"
                     : $"{Site}/files/file/{FileName}/?do=download&r={FileID}";
-                var html = await _downloader.AuthedClient.GetStringAsync(csrfurl);
+                var html = await Downloader.AuthedClient.GetStringAsync(csrfurl);
 
                 var pattern = new Regex("(?<=csrfKey=).*(?=[&\"\'])|(?<=csrfKey: \").*(?=[&\"\'])");
                 var matches = pattern.Matches(html).Cast<Match>();
@@ -126,10 +125,10 @@ namespace Wabbajack.Lib.Downloaders
                     : $"{Site}/files/file/{FileName}/{sep}do=download&r={FileID}&confirm=1&t=1&csrfKey={csrfKey}";
                     
 
-                var streamResult = await _downloader.AuthedClient.GetAsync(url);
+                var streamResult = await Downloader.AuthedClient.GetAsync(url);
                 if (streamResult.StatusCode != HttpStatusCode.OK)
                 {
-                    Utils.ErrorThrow(new InvalidOperationException(), $"{_downloader.SiteName} servers reported an error for file: {FileID}");
+                    Utils.ErrorThrow(new InvalidOperationException(), $"{Downloader.SiteName} servers reported an error for file: {FileID}");
                 }
 
                 var contentType = streamResult.Content.Headers.ContentType;
@@ -142,7 +141,7 @@ namespace Wabbajack.Lib.Downloaders
                 var secs = times.Download - times.CurrentTime;
                 for (int x = 0; x < secs; x++)
                 {
-                    Utils.Status($"Waiting for {secs} at the request of {_downloader.SiteName}", Percent.FactoryPutInRange(x, secs));
+                    Utils.Status($"Waiting for {secs} at the request of {Downloader.SiteName}", Percent.FactoryPutInRange(x, secs));
                     await Task.Delay(1000);
                 }
                 streamResult.Dispose();
@@ -204,7 +203,19 @@ namespace Wabbajack.Lib.Downloaders
                 };
             }
 
-            private static AbstractNeedsLoginDownloader Downloader => (AbstractNeedsLoginDownloader)(object)DownloadDispatcher.GetInstance<TDownloader>();
+            // from AbstractMetaState
+            public override string URL => $"{Site}/files/file/{FileName}";
+            public override string Name { get; set; }
+            public override string Author { get; set; }
+            public override string Version { get; set; }
+            public override string ImageURL { get; set; }
+            public override bool IsNSFW { get; set; }
+            public override string Description { get; set; }
+
+            public override async Task<bool> LoadMetaData()
+            {
+                return false;
+            }
         }
 
         protected AbstractIPS4Downloader(Uri loginUri, string encryptedKeyName, string cookieDomain) : 
--- Wabbajack.Lib/Downloaders/LoversLabDownloader.cs
+++ Wabbajack.Lib/Downloaders/LoversLabDownloader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 using Wabbajack.Common;
 using Wabbajack.Lib.WebAutomation;
 
@@ -12,9 +14,6 @@ namespace Wabbajack.Lib.Downloaders
         public override Uri SiteURL => new Uri("https://www.loverslab.com");
         public override Uri IconUri => new Uri("https://www.loverslab.com/favicon.ico");
         #endregion
-
-        public override string ModNameRegex => "";
-
         public LoversLabDownloader() : base(new Uri("https://www.loverslab.com/login"), 
             "loverslabcookies", "loverslab.com")
         {
@@ -31,15 +30,31 @@ namespace Wabbajack.Lib.Downloaders
                 Utils.Error(ex);
             }
         }
-        public class State : State<LoversLabDownloader>, IMetaState
+
+        public class State : State<LoversLabDownloader>
         {
-            public string URL => $"{Site}/files/file/{FileName}";
-            public string Name { get; }
-            public string Author { get; }
-            public string Version { get; }
-            public string ImageURL { get; }
-            public bool IsNSFW => true;
-            public string Description { get; }
+            public override bool IsNSFW => true;
+
+            public override async Task<bool> LoadMetaData()
+            {
+                var html = await Downloader.AuthedClient.GetStringAsync(URL);
+                var doc = new HtmlDocument();
+                doc.LoadHtml(html);
+                var node = doc.DocumentNode;
+                Name = node.SelectNodes("//h1[@class='ipsType_pageTitle ipsContained_container']/span")?.First().InnerHtml;
+                Author = node
+                    .SelectNodes(
+                        "//div[@class='ipsBox_alt']/div[@class='ipsPhotoPanel ipsPhotoPanel_tiny ipsClearfix ipsSpacer_bottom']/div/p[@class='ipsType_reset ipsType_large ipsType_blendLinks']/a")
+                    ?.First().InnerHtml;
+                Version = node.SelectNodes("//section/h2[@class='ipsType_sectionHead']/span[@data-role='versionTitle']")
+                    ?
+                    .First().InnerHtml;
+                ImageURL = node
+                    .SelectNodes(
+                        "//div[@class='ipsBox ipsSpacer_top ipsSpacer_double']/section/div[@class='ipsPad ipsAreaBackground']/div[@class='ipsCarousel ipsClearfix']/div[@class='ipsCarousel_inner']/ul[@class='cDownloadsCarousel ipsClearfix']/li[@class='ipsCarousel_item ipsAreaBackground_reset ipsPad_half']/span[@class='ipsThumb ipsThumb_medium ipsThumb_bg ipsCursor_pointer']")
+                    ?.First().GetAttributeValue("data-fullurl", "none");
+                return true;
+            }
         }
     }
 }
