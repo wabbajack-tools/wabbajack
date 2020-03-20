ab53e05044cd4d6e877e248e8bdc5a4ee2d9fa19
Timothy Baldridge <tbaldridge@gmail.com>
2020-03-20 17:59:55 -0600

    WIP

---
+++ Wabbajack.Common/Serialization/Deserializer.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Wabbajack.Common.Serialization
+{
+    public class Deserializer
+    {
+        public BinaryReader Reader { get; }
+    }
+}
---
+++ Wabbajack.Common/Serialization/IHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Wabbajack.Common.Serialization
+{
+    public interface IHandler
+    {
+        public void Write<T>(Serializer serializer, T data);
+        public T Read<T>(Deserializer deserialiser);
+
+    }
+}
