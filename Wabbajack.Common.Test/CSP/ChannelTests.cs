a7e220b7798b8d35d10b43ab6ca3a55d4916ab53
Timothy Baldridge <tbaldridge@gmail.com>
2020-03-25 22:34:43 -0600

    Move CSP tests into Commont.Test

--- Wabbajack.Common.Test/CSP/ChannelTests.cs
+++ Wabbajack.Common.Test/CSP/ChannelTests.cs
@@ -1,15 +1,14 @@
 ﻿using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Wabbajack.Common.CSP;
+using Xunit;
 
-namespace Wabbajack.Test.CSP
+namespace Wabbajack.Common.CSP.Test
 {
-    [TestClass]
     public class ChannelTests
     {
-        [TestMethod]
+        [Fact]
         public async Task PutThenTakeNoBuffer()
         {
             var chan = Channel.Create<int>();
@@ -17,11 +16,11 @@ namespace Wabbajack.Test.CSP
             var putter = chan.Put(42);
             var taker = chan.Take();
 
-            Assert.IsTrue(await putter);
-            Assert.AreEqual((true, 42), await taker);
+            Assert.True(await putter);
+            Assert.Equal((true, 42), await taker);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TakeThenPushNoBuffer()
         {
             var chan = Channel.Create<int>();
@@ -29,11 +28,11 @@ namespace Wabbajack.Test.CSP
             var taker = chan.Take();
             var putter = chan.Put(42);
 
-            Assert.IsTrue(await putter);
-            Assert.AreEqual((true, 42), await taker);
+            Assert.True(await putter);
+            Assert.Equal((true, 42), await taker);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TakeFromBufferAfterPut()
         {
             var chan = Channel.Create<int>(1);
@@ -41,11 +40,11 @@ namespace Wabbajack.Test.CSP
             var putter = chan.Put(42);
             var taker = chan.Take();
 
-            Assert.IsTrue(await putter);
-            Assert.AreEqual((true, 42), await taker);
+            Assert.True(await putter);
+            Assert.Equal((true, 42), await taker);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TakeFromBufferBeforePut()
         {
             var chan = Channel.Create<int>(1);
@@ -53,12 +52,12 @@ namespace Wabbajack.Test.CSP
             var taker = chan.Take();
             var putter = chan.Put(42);
 
-            Assert.IsTrue(await putter);
-            Assert.AreEqual((true, 42), await taker);
+            Assert.True(await putter);
+            Assert.Equal((true, 42), await taker);
         }
 
 
-        [TestMethod]
+        [Fact]
         public async Task TakesAreReleasedAfterClose()
         {
             var chan = Channel.Create<int>();
@@ -66,10 +65,10 @@ namespace Wabbajack.Test.CSP
             var taker = chan.Take();
             chan.Close();
 
-            Assert.AreEqual((false, 0), await taker);
+            Assert.Equal((false, 0), await taker);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task ExpandingTransformsReleaseMultipleTakes()
         {
             var chan = Channel.Create<int, int>(1, i => i.SelectMany(len => Enumerable.Range(0, len)));
@@ -78,11 +77,11 @@ namespace Wabbajack.Test.CSP
             var take2 = chan.Take();
             await chan.Put(2);
             
-            Assert.AreEqual((true, 0), await take1);
-            Assert.AreEqual((true, 1), await take2);
+            Assert.Equal((true, 0), await take1);
+            Assert.Equal((true, 1), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanCloseChannel()
         {
             var chan = Channel.Create<int, int>(1, i => i.Take(1));
@@ -93,13 +92,13 @@ namespace Wabbajack.Test.CSP
             await chan.Put(1);
             await chan.Put(2);
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((true, 1), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((true, 1), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanCloseDuringExpand()
         {
             var chan = Channel.Create<int, int>(1, i => i.SelectMany(len => Enumerable.Range(1, len)).Take(1));
@@ -109,13 +108,13 @@ namespace Wabbajack.Test.CSP
 
             await chan.Put(2);
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((true, 1), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((true, 1), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanFilterTakeFirst()
         {
             var chan = Channel.Create<int, int>(1, i => i.Where(x => x == 2));
@@ -127,13 +126,13 @@ namespace Wabbajack.Test.CSP
             await chan.Put(2);
             chan.Close();
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((true, 2), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((true, 2), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanReturnNothingTakeFirst()
         {
             var chan = Channel.Create<int, int>(1, i => i.Take(0));
@@ -143,13 +142,13 @@ namespace Wabbajack.Test.CSP
 
             await chan.Put(1);
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((false, 0), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((false, 0), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanFilterTakeAfter()
         {
             var chan = Channel.Create<int, int>(1, i => i.Where(x => x == 2));
@@ -162,13 +161,13 @@ namespace Wabbajack.Test.CSP
             var take2 = chan.Take();
             chan.Close();
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((true, 2), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((true, 2), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public async Task TransformsCanReturnNothingTakeAfter()
         {
             var chan = Channel.Create<int, int>(1, i => i.Take(0));
@@ -179,37 +178,37 @@ namespace Wabbajack.Test.CSP
             var take2 = chan.Take();
 
 
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(chan.IsClosed);
 
-            Assert.AreEqual((false, 0), await take1);
-            Assert.AreEqual((false, 0), await take2);
+            Assert.Equal((false, 0), await take1);
+            Assert.Equal((false, 0), await take2);
         }
 
-        [TestMethod]
+        [Fact]
         public void TooManyTakesCausesException()
         {
             var chan = Channel.Create<int>();
 
-            Assert.ThrowsException<ManyToManyChannel<int, int>.TooManyHanldersException>(() =>
+            Assert.Throws<ManyToManyChannel<int, int>.TooManyHanldersException>(() =>
             {
                 for (var x = 0; x < ManyToManyChannel<int, int>.MAX_QUEUE_SIZE + 1; x++)
                     chan.Take();
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void TooManyPutsCausesException()
         {
             var chan = Channel.Create<int>();
 
-            Assert.ThrowsException<ManyToManyChannel<int, int>.TooManyHanldersException>(() =>
+            Assert.Throws<ManyToManyChannel<int, int>.TooManyHanldersException>(() =>
             {
                 for (var x = 0; x < ManyToManyChannel<int, int>.MAX_QUEUE_SIZE + 1; x++)
                     chan.Put(x);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public async Task BlockingPutsGoThroughTransform()
         {
             var chan = Channel.Create<int, int>(1, i => i.Take(2));
@@ -225,15 +224,15 @@ namespace Wabbajack.Test.CSP
 
 
 
-            Assert.AreEqual((true, 1), await take1);
-            Assert.AreEqual((true, 2), await take2);
-            Assert.AreEqual((false, 0), await take3);
+            Assert.Equal((true, 1), await take1);
+            Assert.Equal((true, 2), await take2);
+            Assert.Equal((false, 0), await take3);
 
-            Assert.IsTrue(await put1);
-            Assert.IsTrue(await put2);
-            Assert.IsFalse(await put3);
-            Assert.IsFalse(await put4);
-            Assert.IsTrue(chan.IsClosed);
+            Assert.True(await put1);
+            Assert.True(await put2);
+            Assert.False(await put3);
+            Assert.False(await put4);
+            Assert.True(chan.IsClosed);
         }
     }
 }
