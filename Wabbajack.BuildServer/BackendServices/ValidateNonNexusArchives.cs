adbab9576c44031c125ac2baeeedb44df0631269
Timothy Baldridge <tbaldridge@gmail.com>
2020-04-28 15:43:42 -0600

    Fixes for non-Nexus validation

--- Wabbajack.BuildServer/BackendServices/ValidateNonNexusArchives.cs
+++ Wabbajack.BuildServer/BackendServices/ValidateNonNexusArchives.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Wabbajack.BuildServer.Model.Models;
 using Wabbajack.Common;
+using Wabbajack.Lib.Downloaders;
 
 namespace Wabbajack.BuildServer.BackendServices
 {
@@ -13,8 +15,11 @@ namespace Wabbajack.BuildServer.BackendServices
 
         public override async Task Execute()
         {
+            Utils.Log("Updating Non Nexus archives");
             var archives = await Sql.GetNonNexusModlistArchives();
+            Utils.Log($"Validating {archives.Count} Non-Nexus archives.");
             using var queue = new WorkQueue();
+            await DownloadDispatcher.PrepareAll(archives.Select(a => a.State));
             var results = await archives.PMap(queue, async archive =>
             {
                 try
@@ -22,8 +27,9 @@ namespace Wabbajack.BuildServer.BackendServices
                     var isValid = await archive.State.Verify(archive);
                     return (Archive: archive, IsValid: isValid);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
+                    Utils.Log($"Got Validation error {ex}");
                     return (Archive: archive, IsValid: false);
                 }
 
