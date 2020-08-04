abe30fe8a37bb56febb08ee949f434d5069db546
Timothy Baldridge <tbaldridge@gmail.com>
2020-08-03 21:05:19 -0600

    DTOs

---
+++ Wabbajack.Server/DTOs/MirroredFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Wabbajack.Common;
+
+namespace Wabbajack.Server.DTOs
+{
+    public class MirroredFile
+    {
+        public Hash Hash { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Uploaded { get; set; }
+        public string Rationale { get; set; }
+    }
+}
