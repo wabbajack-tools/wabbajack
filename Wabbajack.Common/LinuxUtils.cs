06d568431d127721daf0e9835633fc712853bc91
erri120 <erri120@protonmail.com>
2021-01-02 20:30:13 +0100

    Add LinuxUtils

---
+++ Wabbajack.Common/LinuxUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wabbajack.Common
+{
+    public static class LinuxUtils
+    {
+        public static AbsolutePath GetHomeFolder()
+        {
+            return new (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        }
+    }
+}
