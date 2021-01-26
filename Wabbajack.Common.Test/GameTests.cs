d9902746d09e561a9b0d1e193834b50b8ec2e957
erri120 <erri120@protonmail.com>
2021-01-26 16:14:04 +0100

    Using MO2Name instead of MO2ArchiveName for GameFileSourceDownloader

---
+++ Wabbajack.Common.Test/GameTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace Wabbajack.Common.Test
+{
+    public class GameTests
+    {
+        [Fact]
+        public void TestGetGameFromMO2ArchiveName()
+        {
+            var games = GameRegistry.Games.Select(x => (name: x.Value.MO2Name, game: x.Key));
+            foreach (var (name, game) in games)
+            {
+                if (name == null) continue;
+                var result = GameRegistry.TryGetByFuzzyName(name);
+                Assert.NotNull(result);
+                Assert.Equal(game, result.Game);
+            }
+        }
+    }
+}
