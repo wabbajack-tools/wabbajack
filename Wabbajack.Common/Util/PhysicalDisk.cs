cf29bae7a744a44111f76ce8dc05e4072b60e16f
Unnoen <Unnoen@users.noreply.github.com>
2021-02-04 17:59:09 +1100

    Wrap disk scope in try/catch, default to unspecified if error.

--- Wabbajack.Common/Util/PhysicalDisk.cs
+++ Wabbajack.Common/Util/PhysicalDisk.cs
@@ -12,37 +12,43 @@ namespace Wabbajack.Common
             if (driveLetter.Length == 3 && (driveLetter[1] == ':' && driveLetter[2] == '\\')) driveLetter = driveLetter.Remove(driveLetter.Length - 2);
             if (driveLetter.Length > 3) Utils.Error("Incorrect drive name! Must be X, X: or X:\\");
 
-            Utils.Log($"Phsyical Disk: {driveLetter}");
-
-            // Connect to storage scope
-            var scope = new ManagementScope(@"\\.\root\microsoft\windows\storage");
-            scope.Connect();
-
-            // Search partitions that use requested the drive letter
-            var partitionSearcher = new ManagementObjectSearcher($"SELECT DiskNumber FROM MSFT_Partition WHERE DriveLetter='{driveLetter}'");
-            partitionSearcher.Scope = scope;
-            // Get first partition that matches
-            var partition = partitionSearcher.Get().OfType<ManagementObject>().First();
-
-            // Search for a disk where the device ID matches the one we got from the partition
-            var physicalSearcher = new ManagementObjectSearcher($"SELECT * FROM MSFT_PhysicalDisk WHERE DeviceId='{partition["DiskNumber"]}'");
-            physicalSearcher.Scope = scope;
-            // Get disk information
-            var physical = physicalSearcher.Get().Cast<ManagementBaseObject>().Single();
-
             DriveLetter = driveLetter;
-            DeviceId = (string)physical["DeviceId"];
-            MediaType = (MediaTypes)Convert.ToInt32(physical["MediaType"]);
-            BusType = (BusTypes)Convert.ToInt32(physical["BusType"]);
+
+            try {
+
+                // Connect to storage scope
+                var scope = new ManagementScope(@"\\.\root\microsoft\windows\storage");
+                scope.Connect();
+
+                // Search partitions that use requested the drive letter
+                var partitionSearcher = new ManagementObjectSearcher($"SELECT DiskNumber FROM MSFT_Partition WHERE DriveLetter='{driveLetter}'");
+                partitionSearcher.Scope = scope;
+                // Get first partition that matches
+                var partition = partitionSearcher.Get().OfType<ManagementObject>().First();
+
+                // Search for a disk where the device ID matches the one we got from the partition
+                var physicalSearcher = new ManagementObjectSearcher($"SELECT * FROM MSFT_PhysicalDisk WHERE DeviceId='{partition["DiskNumber"]}'");
+                physicalSearcher.Scope = scope;
+                // Get disk information
+                var physical = physicalSearcher.Get().Cast<ManagementBaseObject>().Single();
+
+                DeviceId = (string)physical["DeviceId"];
+                MediaType = (MediaTypes)Convert.ToInt32(physical["MediaType"]);
+                BusType = (BusTypes)Convert.ToInt32(physical["BusType"]);
+            } 
+            catch (Exception ex)
+            {
+                Utils.Log($"Caught error getting disk info: {ex.Message}. Treating disk as Unspecified.");
+            }
         }
 
         public string DriveLetter { get; }
 
-        public string DeviceId { get;  }
+        public string DeviceId { get; } = "-1";
 
-        public MediaTypes MediaType { get; }
+        public MediaTypes MediaType { get; } = MediaTypes.Unspecified;
 
-        public BusTypes BusType { get; }
+        public BusTypes BusType { get; } = BusTypes.Unknown;
 
         // https://docs.microsoft.com/en-us/previous-versions/windows/desktop/stormgmt/msft-physicaldisk#properties
         public enum MediaTypes
