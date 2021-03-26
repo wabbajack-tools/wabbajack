03b6e6b0d1299e4cc54e6090dd4b4a555fa6a65b
erri120 <erri120@protonmail.com>
2021-03-26 12:35:27 +0100

    Add GameFinder lib

--- Wabbajack.Common/StoreHandlers/StoreHandler.cs
+++ Wabbajack.Common/StoreHandlers/StoreHandler.cs
@@ -2,108 +2,101 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameFinder;
+using GameFinder.StoreHandlers.BethNet;
+using GameFinder.StoreHandlers.EGS;
+using GameFinder.StoreHandlers.GOG;
+using GameFinder.StoreHandlers.Steam;
 
 namespace Wabbajack.Common.StoreHandlers
 {
-    public enum StoreType
-    {
-        STEAM,
-        GOG,
-        BethNet,
-        EpicGameStore,
-        Origin
-    }
-
     public class StoreHandler
     {
-        private static readonly Lazy<StoreHandler> _instance = new Lazy<StoreHandler>(() => new StoreHandler(), isThreadSafe: true);
+        private static readonly Lazy<StoreHandler> _instance = new(() => new StoreHandler(), isThreadSafe: true);
         public static StoreHandler Instance => _instance.Value;
 
-        private static readonly Lazy<SteamHandler> _steamHandler = new Lazy<SteamHandler>(() => new SteamHandler());
+        private static readonly Lazy<SteamHandler> _steamHandler = new(() => new SteamHandler());
         public SteamHandler SteamHandler = _steamHandler.Value;
 
-        private static readonly Lazy<GOGHandler> _gogHandler = new Lazy<GOGHandler>(() => new GOGHandler());
+        private static readonly Lazy<GOGHandler> _gogHandler = new(() => new GOGHandler());
         public GOGHandler GOGHandler = _gogHandler.Value;
 
-        private static readonly Lazy<BethNetHandler> _bethNetHandler = new Lazy<BethNetHandler>(() => new BethNetHandler());
+        private static readonly Lazy<BethNetHandler> _bethNetHandler = new(() => new BethNetHandler());
         public BethNetHandler BethNetHandler = _bethNetHandler.Value;
         
-        private static readonly Lazy<EpicGameStoreHandler> _epicGameStoreHandler = new Lazy<EpicGameStoreHandler>(() => new EpicGameStoreHandler());
-        public EpicGameStoreHandler EpicGameStoreHandler = _epicGameStoreHandler.Value;
+        private static readonly Lazy<EGSHandler> _epicGameStoreHandler = new(() => new EGSHandler());
+        public EGSHandler EpicGameStoreHandler = _epicGameStoreHandler.Value;
         
-        private static readonly Lazy<OriginHandler> _originHandler = new Lazy<OriginHandler>(() => new OriginHandler());
+        private static readonly Lazy<OriginHandler> _originHandler = new(() => new OriginHandler());
         public OriginHandler OriginHandler = _originHandler.Value;
 
         public List<AStoreGame> StoreGames;
 
+        public Dictionary<Game, AStoreGame> Games = new();
+        
+        private void FindGames<THandler, TGame>(THandler handler, string name) 
+            where THandler : AStoreHandler<TGame> 
+            where TGame : AStoreGame
+        {
+            try
+            {
+                handler.FindAllGames();
+                foreach (var game in handler.Games)
+                {
+                    Utils.Log($"{handler.StoreType}: Found game {game}");
+                    StoreGames.Add(game);
+                }
+            }
+            catch (Exception e)
+            {
+                Utils.Error(e, $"Could not load all Games from the {name}");
+            }
+        }
+        
         public StoreHandler()
         {
             StoreGames = new List<AStoreGame>();
-
-            if (SteamHandler.Init())
-            {
-                if(SteamHandler.LoadAllGames())
-                    StoreGames.AddRange(SteamHandler.Games);
-                else
-                    Utils.Error(new StoreException("Could not load all Games from the SteamHandler, check previous error messages!"));
-            }
-            else
-            {
-                Utils.Error(new StoreException("Could not Init the SteamHandler, check previous error messages!"));
-            }
-
-            if (GOGHandler.Init())
-            {
-                if(GOGHandler.LoadAllGames())
-                    StoreGames.AddRange(GOGHandler.Games);
-                else
-                    Utils.Error(new StoreException("Could not load all Games from the GOGHandler, check previous error messages!"));
-            }
-            else
-            {
-                Utils.Error(new StoreException("Could not Init the GOGHandler, check previous error messages!"));
-            }
-
-            if (BethNetHandler.Init())
-            {
-                if (BethNetHandler.LoadAllGames())
-                    StoreGames.AddRange(BethNetHandler.Games);
-                else
-                    Utils.Error(new StoreException("Could not load all Games from the BethNetHandler, check previous error messages!"));
-            }
-            else
-            {
-                Utils.Error(new StoreException("Could not Init the BethNetHandler, check previous error messages!"));
-            }
             
-            if (EpicGameStoreHandler.Init())
-            {
-                if (EpicGameStoreHandler.LoadAllGames())
-                    StoreGames.AddRange(EpicGameStoreHandler.Games);
-                else
-                    Utils.Error(new StoreException("Could not load all Games from the EpicGameStoreHandler, check previous error messages!"));
-            }
-            else
-            {
-                Utils.Error(new StoreException("Could not Init the EpicGameStoreHandler, check previous error messages!"));
-            }
+            FindGames<SteamHandler, SteamGame>(SteamHandler, "SteamHandler");
+            FindGames<GOGHandler, GOGGame>(GOGHandler, "GOGHandler");
+            FindGames<BethNetHandler, BethNetGame>(BethNetHandler, "BethNetHandler");
+            FindGames<EGSHandler, EGSGame>(EpicGameStoreHandler, "EpicGameStoreHandler");
             
             if (OriginHandler.Init())
             {
-                if (OriginHandler.LoadAllGames())
-                    StoreGames.AddRange(OriginHandler.Games);
-                else
+                if (!OriginHandler.LoadAllGames())
                     Utils.Error(new StoreException("Could not load all Games from the OriginHandler, check previous error messages!"));
             }
             else
             {
                 Utils.Error(new StoreException("Could not Init the OriginHandler, check previous error messages!"));
             }
+
+            foreach (var storeGame in StoreGames)
+            {
+                IEnumerable<KeyValuePair<Game, GameMetaData>>? enumerable = storeGame switch
+                {
+                    SteamGame steamGame => GameRegistry.Games.Where(y => y.Value.SteamIDs?.Contains(steamGame.ID) ?? false),
+                    GOGGame gogGame => GameRegistry.Games.Where(y => y.Value.GOGIDs?.Contains(gogGame.GameID) ?? false),
+                    BethNetGame bethNetGame => GameRegistry.Games.Where(y => y.Value.BethNetID.Equals((int)bethNetGame.ID)),
+                    EGSGame egsGame => GameRegistry.Games.Where(y => y.Value.EpicGameStoreIDs.Contains(egsGame.CatalogItemId ?? string.Empty)),
+                    _ => null
+                };
+
+                if (enumerable == null) continue;
+                
+                var list = enumerable.ToList();
+                if (list.Count == 0) continue;
+                
+                Games.Add(list.First().Key, storeGame);
+            }
         }
 
         public AbsolutePath? TryGetGamePath(Game game)
         {
-            return StoreGames.FirstOrDefault(g => g.Game == game)?.Path;
+            if (Games.TryGetValue(game, out var storeGame))
+                return (AbsolutePath) storeGame.Path;
+            return OriginHandler.Games.FirstOrDefault(x => x.Game == game)?.Path;
         }
 
         public static void Warmup()
@@ -111,27 +104,7 @@ namespace Wabbajack.Common.StoreHandlers
             Task.Run(() => _instance.Value).FireAndForget();
         }
     }
-
-    public abstract class AStoreGame
-    {
-        public abstract Game Game { get; internal set; }
-        public virtual string Name { get; internal set; } = string.Empty;
-        public virtual AbsolutePath Path { get; internal set; }
-        public virtual int ID { get; internal set; }
-        public abstract StoreType Type { get; internal set; }
-    }
-
-    public abstract class AStoreHandler
-    {
-        public List<AStoreGame> Games { get; } = new List<AStoreGame>();
-
-        public abstract StoreType Type { get; internal set; }
-
-        public abstract bool Init();
-
-        public abstract bool LoadAllGames();
-    }
-
+    
     public class StoreException : Exception
     {
         public StoreException(string msg) : base(msg)
