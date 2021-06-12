e9cf0dcc5f21b662d9a94bbf2066e5b72d2fdd55
erri120 <erri120@protonmail.com>
2021-06-12 20:33:04 +0200

    Web: Add IHttpClientFactory injection

--- Wabbajack.Web/Startup.cs
+++ Wabbajack.Web/Startup.cs
@@ -25,6 +25,7 @@ namespace Wabbajack.Web
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddMudServices();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
