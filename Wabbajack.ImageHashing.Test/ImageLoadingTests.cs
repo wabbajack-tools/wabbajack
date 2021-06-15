c9a2ea8180137a9659624d4db1cb168065ced1f6
Timothy Baldridge <tbaldridge@gmail.com>
2021-06-14 21:39:17 -0600

    Image hashing initial work

---
+++ Wabbajack.ImageHashing.Test/ImageLoadingTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Wabbajack.Common;
+using Xunit;
+
+namespace Wabbajack.ImageHashing.Test
+{
+    public class ImageLoadingTests
+    {
+        [Fact]
+        public async Task CanLoadDDSFiles()
+        {
+            var file = DDSImage.FromFile(AbsolutePath.EntryPoint.Combine("Resources", "test-dxt5.dds"));
+        }
+    }
+}
---
+++ Wabbajack.ImageHashing/DDSImage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DirectXTexNet;
+using Wabbajack.Common;
+
+namespace Wabbajack.ImageHashing
+{
+    public class DDSImage : IImage
+    {
+        private DDSImage()
+        {
+            
+        }
+
+        private static Extension DDSExtension = new(".dds");
+        private ScratchImage _image;
+        private TexMetadata _metaData;
+
+        public static DDSImage FromFile(AbsolutePath file)
+        {
+            if (file.Extension != DDSExtension)
+                throw new Exception("File does not end in DDS");
+
+            var img = TexHelper.Instance.LoadFromDDSFile(file.ToString(), DDS_FLAGS.NONE);
+            
+            return new DDSImage() {_image = img, _metaData = img.GetMetadata()};
+        }
+
+        public void Dispose()
+        {
+            if (!_image.IsDisposed) 
+                _image.Dispose();
+        }
+
+        public int Width => _metaData.Width;
+        public int Height => _metaData.Height;
+        public GPUCompressionLevel CompressionLevel => GPUCompressionLevel.Uncompressed;
+        public IImageState State { get; }
+    }
+}
