402113803b8895a2396eb458089663bb8635a7fd
erri120 <erri120@protonmail.com>
2021-06-26 12:19:01 +0200

    Web: Update

--- Wabbajack.Web/Services/StateContainer.cs
+++ Wabbajack.Web/Services/StateContainer.cs
@@ -15,19 +15,28 @@ namespace Wabbajack.Web.Services
     public class StateContainer
     {
         private const string ModlistsJsonUrl = "https://raw.githubusercontent.com/wabbajack-tools/mod-lists/master/modlists.json";
+        private const string ModlistStatusListUrl = "https://build.wabbajack.org/lists/status.json";
+        
         private readonly HttpClient _client;
+        private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions
+        {
+            IgnoreNullValues = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true
+        };
 
         public StateContainer(HttpClient client)
         {
             _client = client;
         }
+
+        #region ModlistMetadata
         
         private List<ModlistMetadata> _modlists = new();
         public IReadOnlyList<ModlistMetadata> Modlists => _modlists;
-
         public bool HasModlistMetadata => _modlists.Any();
-
-        public ModlistMetadata? GetByMachineUrl(string machineUrl)
+        
+        public ModlistMetadata? GetModlistByMachineUrl(string machineUrl)
         {
             return _modlists.FirstOrDefault(x =>
                 x.Links?.MachineUrl != null &&
@@ -45,13 +54,49 @@ namespace Wabbajack.Web.Services
         
         private async Task<List<ModlistMetadata>?> GetModlistMetadata()
         {
-            var res = await _client.GetFromJsonAsync<List<ModlistMetadata>>(ModlistsJsonUrl, new JsonSerializerOptions
-            {
-                ReadCommentHandling = JsonCommentHandling.Skip,
-                IgnoreNullValues = true,
-            }, CancellationToken.None);
+            var res = await _client.GetFromJsonAsync<List<ModlistMetadata>>(
+                ModlistsJsonUrl,
+                JsonSerializerOptions,
+                CancellationToken.None);
 
             return res;
         }
+
+        #endregion
+
+        #region ModlistStatus
+
+        private List<ModlistStatus> _modlistStatusList = new();
+        public IReadOnlyList<ModlistStatus> ModlistStatusList => _modlistStatusList;
+        public bool HasModlistStatusList => _modlistStatusList.Any();
+
+        public ModlistStatus? GetStatusByMachineUrl(string machineUrl)
+        {
+            return _modlistStatusList.FirstOrDefault(x =>
+                x.MachineUrl != null &&
+                x.MachineUrl.Equals(machineUrl, StringComparison.OrdinalIgnoreCase));
+        }
+        
+        public async Task<bool> RefreshModlistStatus()
+        {
+            var res = await GetModlistStatusList();
+            if (res == null) return false;
+            
+            _modlistStatusList = res;
+            return true;
+        }
+        
+        private async Task<List<ModlistStatus>?> GetModlistStatusList()
+        {
+            var res = await _client.GetFromJsonAsync<List<ModlistStatus>>(
+                ModlistStatusListUrl,
+                JsonSerializerOptions,
+                CancellationToken.None);
+
+            return res;
+        }
+        
+        #endregion
+        
     }
 }
