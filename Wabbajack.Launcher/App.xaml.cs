03b3ec498dd17fe824c1edc53a9cb9657beeddca
Timothy Baldridge <tbaldridge@gmail.com>
2021-10-02 11:01:09 -0600

    Wip for 3.0 launcher

---
+++ Wabbajack.Launcher/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia;
+
+namespace Wabbajack.Launcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Wabbajack.Launcher/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Avalonia.Controls;
+
+namespace Wabbajack.Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            try
+            {
+                DataContext = new MainWindowVM();
+            }
+            catch(Exception ex)
+            {
+                System.Console.Error.WriteLine("Error creating datacontext.");
+                System.Console.Error.WriteLine(ex);
+                throw;
+            }
+        }
+    }
+}
