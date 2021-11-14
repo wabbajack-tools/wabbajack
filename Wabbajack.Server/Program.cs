e1790be84f89614b8bcfa0d604db0b451180c33a
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2021-11-14 16:58:24 +0100

    Update Program.cs

--- Wabbajack.Server/Program.cs
+++ Wabbajack.Server/Program.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using NetworkPorts;
 
 namespace Wabbajack.Server;
 
@@ -22,9 +23,9 @@ public class Program
                 webBuilder.UseStartup<Startup>()
                     .UseKestrel(options =>
                     {
-                        options.Listen(IPAddress.Any, testMode ? 8080 : 80);
+                        options.Listen(IPAddress.Any, testMode ? 8080 : NetworkPort.Http);
                         if (!testMode)
-                            options.Listen(IPAddress.Any, 443, listenOptions =>
+                            options.Listen(IPAddress.Any, NetworkPort.Https, listenOptions =>
                             {
                                 using var store = new X509Store(StoreName.My);
                                 store.Open(OpenFlags.ReadOnly);
