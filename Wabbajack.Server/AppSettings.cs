892948eebc3e225f822789c04fb48f97f350efe2
Timothy Baldridge <tbaldridge@gmail.com>
2021-11-27 13:18:41 -0700

    update settings

--- Wabbajack.Server/AppSettings.cs
+++ Wabbajack.Server/AppSettings.cs
@@ -19,7 +19,7 @@ public class AppSettings
     public string HamWebHook { get; set; } = null;
     
     public string AuthoredFilesFolder { get; set; }
-    public string MetricsFolder { get; set; }
+    public string MetricsFolder { get; set; } = "";
     public string TarLogPath { get; set; }
-    public string GitHubKey { get; set; }
+    public string GitHubKey { get; set; } = "";
 }
