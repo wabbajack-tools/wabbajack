7419fc75a59697a5c21429a8c9a7c294b5debab4
Unnoen <Unnoen@users.noreply.github.com>
2022-01-10 00:33:22 +1100

    We got something going on.

---
+++ Wabbajack.App.Blazor/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Wabbajack.Services.OSIntegrated;
+
+namespace Wabbajack.App.Blazor
+{
+    public partial class App
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IHost            _host;
+
+        public App()
+        {
+            _host = Host.CreateDefaultBuilder(Array.Empty<string>())
+                .ConfigureLogging(c => { c.ClearProviders(); })
+                .ConfigureServices((host, services) => { ConfigureServices(services); })
+                .Build();
+
+            _serviceProvider = _host.Services;
+        }
+
+        private static IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddBlazorWebView();
+            services.AddOSIntegrated();
+            services.AddTransient<MainWindow>();
+            return services;
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow!.Show();
+        }
+
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+            Current.Shutdown();
+            // using (_host)
+            // {
+            //     _host.StopAsync();
+            // }
+            //
+            // base.OnExit(e);
+        }
+    }
+}
---
+++ Wabbajack.App.Blazor/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Wabbajack.App.Blazor
+{
+    public partial class MainWindow
+    {
+        private readonly ILogger<MainWindow> _logger;
+        public MainWindow(ILogger<MainWindow> logger, IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            Resources.Add("services", serviceProvider);
+            InitializeComponent();
+        }
+    }
+    
+    // Required so compiler doesn't complain about not finding the type. [MC3050]
+    public partial class Main {}
+}
