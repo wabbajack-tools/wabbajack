872cb5e34c754f5daae2cb9023b0c4c3564fb37b
Timothy Baldridge <tbaldridge@gmail.com>
2022-01-21 15:20:47 -0700

    Add UIUtils, link links on the sidebar

---
+++ Wabbajack.App.Blazor/Utility/UIUtils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+using Wabbajack.Hashing.xxHash64;
+using Wabbajack.Paths;
+using Wabbajack.Paths.IO;
+
+namespace Wabbajack
+{
+    public static class UIUtils
+    {
+        public static void OpenWebsite(Uri url)
+        {
+            Process.Start(new ProcessStartInfo("cmd.exe", $"/c start {url}")
+            {
+                CreateNoWindow = true,
+            });
+        }
+        
+        public static void OpenFolder(AbsolutePath path)
+        {
+            Process.Start(new ProcessStartInfo(KnownFolders.Windows.Combine("explorer.exe").ToString(), path.ToString())
+            {
+                CreateNoWindow = true,
+            });
+        }
+
+
+        public static AbsolutePath OpenFileDialog(string filter, string initialDirectory = null)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = filter;
+            ofd.InitialDirectory = initialDirectory;
+            if (ofd.ShowDialog() == DialogResult.OK)
+                return (AbsolutePath)ofd.FileName;
+            return default;
+        }
+        
+    }
+}
