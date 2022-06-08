790f7cd7aad3c35511c33f50cc0a783184e3a3b2
Timothy Baldridge <tbaldridge@gmail.com>
2022-06-07 21:51:39 -0600

    Fix downloader tests

--- Wabbajack.Downloaders.Dispatcher.Test/Startup.cs
+++ Wabbajack.Downloaders.Dispatcher.Test/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Wabbajack.DTOs.Interventions;
 using Wabbajack.Services.OSIntegrated;
 using Xunit.DependencyInjection;
 using Xunit.DependencyInjection.Logging;
@@ -11,10 +12,19 @@ public class Startup
     public void ConfigureServices(IServiceCollection service)
     {
         service.AddOSIntegrated();
+        service.AddSingleton<IUserInterventionHandler, CancellingInterventionHandler>();
     }
 
     public void Configure(ILoggerFactory loggerFactory, ITestOutputHelperAccessor accessor)
     {
         loggerFactory.AddProvider(new XunitTestOutputLoggerProvider(accessor, delegate { return true; }));
+    }
+
+    private class CancellingInterventionHandler : IUserInterventionHandler
+    {
+        public void Raise(IUserIntervention intervention)
+        {
+            intervention.Cancel();
+        }
     }
 }
