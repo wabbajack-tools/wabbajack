c60c05bbf7eac14c33cfb5f9cd08637186099134
erri120 <erri120@protonmail.com>
2022-11-10 15:23:30 +0100

    Hide metas with removed=false

--- Wabbajack.Installer/StandardInstaller.cs
+++ Wabbajack.Installer/StandardInstaller.cs
@@ -248,7 +248,9 @@ public class StandardInstaller : AInstaller<StandardInstaller>
                         try
                         {
                             var parsed = metaFile.LoadIniFile();
-                            if (parsed["General"] is not null && (parsed["General"]["removed"] is null || parsed[General][removed=false]))
+                            if (parsed["General"] is not null && (
+                                    parsed["General"]["removed"] is null ||
+                                    parsed["General"]["removed"].Equals(bool.FalseString, StringComparison.OrdinalIgnoreCase)))
                             {
                                 // add removed=true to files not part of the Modlist so they don't show up in MO2
                                 parsed["General"]["removed"] = "true";
