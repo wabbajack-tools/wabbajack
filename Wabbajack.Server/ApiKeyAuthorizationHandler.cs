59126d1f259caaa11e0c7b47bb474032f27a29ce
Halgari <tbaldridge@gmail.com>
2023-02-23 21:39:42 -0700

    Add tarlog support for author keys

--- Wabbajack.Server/ApiKeyAuthorizationHandler.cs
+++ Wabbajack.Server/ApiKeyAuthorizationHandler.cs
@@ -86,6 +86,9 @@ public class ApiKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthentic
         
         if (authorKey != null)
         {
+            if (await _tarLog.Contains(authorKey))
+                return AuthenticateResult.Fail("Banned author key");
+            
             var owner = await _authorKeys.AuthorForKey(authorKey);
             if (owner == null)
             {
