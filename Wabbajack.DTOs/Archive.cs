2fcad645521d00bdf7d732a9532e4e9a7f5790dc
Alex <alexjdickerson1995@gmail.com>
2024-12-27 22:38:05 -0500

    Reverting unneeded changes

--- Wabbajack.DTOs/Archive.cs
+++ Wabbajack.DTOs/Archive.cs
@@ -1,24 +1,13 @@
-using System;
 using Wabbajack.DTOs.DownloadStates;
 using Wabbajack.Hashing.xxHash64;
 
 namespace Wabbajack.DTOs;
 
-public class Archive : IComparable
+public class Archive
 {
     public Hash Hash { get; set; }
     public string Meta { get; set; } = "";
     public string Name { get; set; }
     public long Size { get; set; }
     public IDownloadState State { get; set; }
-
-    public int CompareTo(object obj)
-    {
-        if (obj == null) return 1;
-        Archive otherArchive = obj as Archive;
-        if (otherArchive != null)
-            return this.Size.CompareTo(otherArchive.Size);
-        else
-            throw new ArgumentException("Object is not an Archive");
-    }
 }
