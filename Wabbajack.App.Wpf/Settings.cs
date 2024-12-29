688f60157dc7d6773af179d4a9a559299f45be28
Alex <alexjdickerson1995@gmail.com>
2024-12-28 22:15:04 -0500

    Removed obsolete setting and associated ui elements

--- Wabbajack.App.Wpf/Settings.cs
+++ Wabbajack.App.Wpf/Settings.cs
@@ -17,38 +17,12 @@ namespace Wabbajack
     public class PerformanceSettings : ViewModel
     {
         private readonly Configuration.MainSettings _settings;
-        private readonly int _defaultMaximumMemoryPerDownloadThreadMb;
 
         public PerformanceSettings(Configuration.MainSettings settings, IResource<DownloadDispatcher> downloadResources, SystemParametersConstructor systemParams)
         {
             var p = systemParams.Create();
 
             _settings = settings;
-            // Split half of available memory among download threads
-            _defaultMaximumMemoryPerDownloadThreadMb = (int)(p.SystemMemorySize / downloadResources.MaxTasks / 1024 / 1024) / 2;
-            _maximumMemoryPerDownloadThreadMb = settings.PerformanceSettings.MaximumMemoryPerDownloadThreadMb;
-
-            if (MaximumMemoryPerDownloadThreadMb < 0)
-            {
-                ResetMaximumMemoryPerDownloadThreadMb();
-            }
-        }
-
-        private int _maximumMemoryPerDownloadThreadMb;
-
-        public int MaximumMemoryPerDownloadThreadMb
-        {
-            get => _maximumMemoryPerDownloadThreadMb;
-            set
-            {
-                RaiseAndSetIfChanged(ref _maximumMemoryPerDownloadThreadMb, value);
-                _settings.PerformanceSettings.MaximumMemoryPerDownloadThreadMb = value;
-            }
-        }
-
-        public void ResetMaximumMemoryPerDownloadThreadMb()
-        {
-            MaximumMemoryPerDownloadThreadMb = _defaultMaximumMemoryPerDownloadThreadMb;
         }
     }
 }
