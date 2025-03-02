70b74233cfaeb32ebd01b510e9c7bc69dba2f956
trawzified <55751269+tr4wzified@users.noreply.github.com>
2025-03-02 17:49:57 +0100

    Add NSFW and Unavailable tags to gallery, overhaul unavailable styling of modlist tiles

---
+++ Wabbajack.App.Wpf/Converters/MultiBoolToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Wabbajack;
+
+public class MultiBoolToVisibilityConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (values.All(v => v is bool b && b)) return Visibility.Visible;
+        return Visibility.Collapsed;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException(); // We are not implementing the ConvertBack method here
+    }
+}
