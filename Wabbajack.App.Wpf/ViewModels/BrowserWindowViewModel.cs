37f4720a22de9569fcb3e84a50473613b79d57b0
trawzified <55751269+tr4wzified@users.noreply.github.com>
2025-06-21 11:46:35 +0200

    Revert "Focus main window after closing popout browser window"
    This reverts commit 8d6a81e0407c25cb1c939c26fc76356920669487.

--- Wabbajack.App.Wpf/ViewModels/BrowserWindowViewModel.cs
+++ Wabbajack.App.Wpf/ViewModels/BrowserWindowViewModel.cs
@@ -63,8 +63,6 @@ public abstract class BrowserWindowViewModel : ViewModel, IClosableVM
                     {
                         ShowBrowserWindow.Send(this, openExistingOperation: true);
                         PoppedOut = false;
-                        Application.Current.MainWindow.WindowState = WindowState.Normal;
-                        Application.Current.MainWindow.Focus();
                     }
                 };
                 PoppedOut = true;
