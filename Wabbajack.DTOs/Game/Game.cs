e4134d5a1215203755ac3e31135db46139150a3f
trawzified <55751269+tr4wzified@users.noreply.github.com>
2025-06-21 12:00:25 +0200

    Add FO76 back

--- Wabbajack.DTOs/Game/Game.cs
+++ Wabbajack.DTOs/Game/Game.cs
@@ -42,5 +42,6 @@ public enum Game
     [Description("Starfield")] Starfield,
     [Description("7 Days to Die")] SevenDaysToDie,
     [Description("Oblivion Remastered")] OblivionRemastered,
+    [Description("Fallout 76")] Fallout76,
     [Description("Fallout 4: London")] Fallout4London,
 }
