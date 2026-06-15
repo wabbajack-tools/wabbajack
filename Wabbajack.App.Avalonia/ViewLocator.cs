5622f8f4ebf8584e50f42a14d2bf447c719c9de2
Timothy Baldridge <tbaldridge@gmail.com>
2026-06-15 14:35:27 -0700

    Avalonia Wave 0: ViewLocator replace-order + Avalonia.ReactiveUI version consistency

--- Wabbajack.App.Avalonia/ViewLocator.cs
+++ Wabbajack.App.Avalonia/ViewLocator.cs
@@ -10,7 +10,7 @@ public class ViewLocator : IDataTemplate
     public Control Build(object? data)
     {
         if (data is null) return new TextBlock { Text = "null" };
-        var name = data.GetType().FullName!.Replace("VM", "View").Replace("ViewModel", "View");
+        var name = data.GetType().FullName!.Replace("ViewModel", "View").Replace("VM", "View");
         var type = Type.GetType(name) ?? Type.GetType(name + ", WabbajackAvalonia");
         if (type != null) return (Control) Activator.CreateInstance(type)!;
         return new TextBlock { Text = "Not Found: " + name };
